682e69b65338f2359334302a67f90b43c1b76228
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-02-26 01:06:27 +0100

    Compare RAW and JPEG

--- compare_raw_jpeg.py
+++ compare_raw_jpeg.py
@@ -11,26 +11,26 @@ from sys import argv
 from matplotlib import pyplot as plt
 from spectacle import io
 from astropy import table
-from datetime import datetime
-from os import walk
+from wk import hydrocolor as hc
 
 # Get the data folder from the command line
 path_raw, path_jpeg = io.path_from_input(argv)
+phone_name = " ".join(path_raw.stem.split("_")[1:-1])
 
-table_raw = table.Table.read(path_raw)
-table_jpeg = table.Table.read(path_jpeg)
+data_raw = table.Table.read(path_raw)
+data_jpeg = table.Table.read(path_jpeg)
 
-max_val = 0
+parameters = ["Lu", "Lsky", "Ld", "Ed", "Rrs"]
+labels = ["$L_u$ [ADU nm$^{-1}$ sr$^{-1}$]", "$L_{sky}$ [ADU nm$^{-1}$ sr$^{-1}$]", "$L_d$ [ADU nm$^{-1}$ sr$^{-1}$]", "$E_d$ [ADU nm$^{-1}$]", "$R_{rs}$ [sr$^{-1}$]"]
 
-plt.figure(figsize=(5,5), tight_layout=True)
-for c in "RGB":
-    plt.errorbar(table_raw[f"R_rs ({c})"], table_jpeg[f"R_rs ({c})"], xerr=table_raw[f"R_rs_err ({c})"], yerr=table_jpeg[f"R_rs_err ({c})"], color=c, fmt="o")
-    max_val = max(max_val, table_raw[f"R_rs ({c})"].max(), table_jpeg[f"R_rs ({c})"].max())
-plt.plot([-1, 1], [-1, 1], c='k', ls="--")
-plt.xlim(0, 1.05*max_val)
-plt.ylim(0, 1.05*max_val)
-plt.grid(True, ls="--")
-plt.xlabel("RAW $R_{rs}$ [sr$^{-1}$]")
-plt.ylabel("JPEG $R_{rs}$ [sr$^{-1}$]")
-plt.savefig(f"comparison_RAW_X_JPEG.pdf")
-plt.show()
+for param, label in zip(parameters, labels):
+    aspect = (param == "Rrs")
+
+    RMS_all, RMS_RGB = hc.RMS_RGB(data_raw, data_jpeg, param)
+    r_all, r_RGB = hc.correlation_RGB(data_raw, data_jpeg, param)
+
+    title_r = f"$r$ = {r_all:.2f}"
+    title_RMS = f"    RMSE = {RMS_all:.3f} sr$" + "^{-1}$" if param == "Rrs" else ""
+    title = f"{title_r} {title_RMS}"
+
+    hc.correlation_plot_RGB(data_raw, data_jpeg, param+" {c}", param+" {c}", xerrlabel=param+"_err {c}", yerrlabel=param+"_err {c}", xlabel=f"RAW {label}", ylabel=f"JPEG {label}", title=title, equal_aspect=aspect, saveto=f"results/comparison_{phone_name}_RAW_X_JPEG_{param}.pdf")
