cf44973400545c80514e47b7f9124dce90f303de
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-06-26 02:01:44 +0200

    Refactor parameters of interest

--- compare_phone_reference.py
+++ compare_phone_reference.py
@@ -63,9 +63,12 @@ effective_bandwidths = camera.spectral_bands
 table_phone = hc.read_results(path_phone)
 table_reference = table.Table.read(path_reference)
 
+# Parameters of interest
+parameters = ["Ed", "Lsky", "Lu", "R_rs"]
+
 # Spectral convolution
 # Convolve R_rs itself for now because of fingerprinting
-for key in ["Ed", "Lsky", "Lu", "R_rs"]:
+for key in parameters:
     cols = [col for col in table_reference.keys() if key in col and not any(f"({label})" in col for label in [*"XYZxy", "hue", "FU"])]  # Find the relevant keys
     wavelengths = np.array([float(col.split(key)[1][1:]) for col in cols])  # Data wavelengths
     data = np.array(table_reference[cols]).view(np.float64).reshape((-1, len(wavelengths)))  # Cast the relevant data to a numpy array
@@ -108,7 +111,7 @@ for row in table_phone:  # Loop over the smartphone table to look for matches
 
     # Calculate the median Lu/Lsky/Ed/R_rs within the matching observations, and uncertainty on this spectrum
     row_reference = table.Table(table_reference[closest])
-    for key in ["Ed", "Lu", "Lsky", "R_rs"]:
+    for key in parameters:
         # Average over the "close enough" rows
         keys = [f"{key}_{wvl:.1f}" for wvl in wavelengths] + hc.extend_keys_to_RGB([key])
         keys_err = [f"{key}_err_{wvl:.1f}" for wvl in wavelengths] + hc.extend_keys_to_RGB([key+"_err"])
