c9b81998d9395e0c03b12e4bee2bc3713850fa8c
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-09-12 16:55:42 +0200

    Load Rrs data, convert timestamps

--- process_switzerland.py
+++ process_switzerland.py
@@ -22,10 +22,24 @@ from wk import wacodi as wa, hydrocolor as hc
 folder = Path(argv[1])
 print("Input folder:", folder.absolute())
 saveto = Path("water-colour-data/data_switzerland_table.csv")
-
+trios_filename = "Rrs_output_simis.csv"
 
 # Loop over the data folders for each lake
 data_folders = hc.generate_folders([folder], "RAMSES_calibrated")
 
 for folder in data_folders:
-    pass
+    # Filename in this folder
+    Rrs_filename = folder / trios_filename
+
+    # Load the data
+    header = np.genfromtxt(Rrs_filename, delimiter=",", dtype=str, max_rows=1)  # Contains timestamps
+    data = np.genfromtxt(Rrs_filename, delimiter=",", dtype=float)  # Contains radiometry
+
+    # Convert header into timestamps and UTC format
+    header = header[1:-3]  # Remove wavelength and percentile columns
+    timestamps = [h.replace(" ", "T") for h in header]
+    UTC = [datetime.fromisoformat(t).timestamp() for t in timestamps]
+
+    # Transpose the data and convert everything to astropy tables
+    timestamps = table.Column(name="DateTime", data=timestamps)
+    UTC = table.Column(name="UTC", data=UTC)
