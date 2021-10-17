880e8884ce86b9c2032e54a24a5789bacdeb7288
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-10-17 02:42:54 +0200

    Update script

--- flowchart.py
+++ flowchart.py
@@ -53,14 +53,15 @@ print("Loaded RAW data")
 # Load and plot JPEG images
 jpeg_paths = hc.generate_paths(data_path, ".JPG")
 images_jpeg = hc.load_jpeg_images(jpeg_paths)
-images_jpeg = [np.moveaxis(img, 0, 1) for img in images_jpeg]  # Rotate image
+images_jpeg = np.moveaxis(images_jpeg, 1, -1)  # Move RGB axis to the end
+images_jpeg = np.moveaxis(images_jpeg, 1, 2)  # Rotate images
 plot.plot_three_images(images_jpeg, saveto=saveto/"images.pdf")
 print("Plotted JPEG images")
 
 # Plot the thumbnails
 
 # Correct for bias
-images_bias_corrected = camera.correct_bias(*images_raw)
+images_bias_corrected = camera.correct_bias(images_raw)
 print("Corrected bias")
 
 # Normalising for ISO speed is not necessary since this is a relative measurement
@@ -68,21 +69,22 @@ print("Corrected bias")
 # Dark current is negligible
 
 # Correct for flat-field
-images_flatfield_corrected = camera.correct_flatfield(*images_bias_corrected)
+images_flatfield_corrected = camera.correct_flatfield(images_bias_corrected)
 print("Corrected flat-field")
 
-# Demosaick the data
-images_RGBG = camera.demosaick(*images_flatfield_corrected)
-print("Demosaicked")
-
 # Select the central pixels
-images_central_slices = hc.central_slice_raw(*images_RGBG)
+central_slice = camera.central_slice(100, 100)
+images_central_slices = images_flatfield_corrected[central_slice]
+
+# Demosaick the data
+images_RGBG = camera.demosaick(images_central_slices, selection=central_slice)
+print("Demosaicked")
 
 # Plot images of the data reduction so far
 plot.plot_image_small(images_raw, saveto=saveto/"rawimage_{label}.pdf")
 plot.plot_image_small(images_flatfield_corrected, saveto=saveto/"flatimage_{label}.pdf")
+plot.plot_image_small(images_central_slices, saveto=saveto/"slice_{label}.pdf")  # Make this use the same vmin/vmax as above
 plot.plot_image_small_RGBG2(images_RGBG, camera, saveto=saveto/"RGBG2_{label}.pdf")
-plot.plot_image_small_RGBG2(images_central_slices, camera, saveto=saveto/"slice_{label}.pdf")
 
 # Reshape the central images to lists
 data_RGBG = np.array([image.reshape(4, -1) for image in images_central_slices])
@@ -146,40 +148,35 @@ hc.print_bandratios(bandratios, bandratios_covariance)
 # WACODI
 
 # Convert RGB to XYZ
-water_XYZ, sky_XYZ, card_XYZ, R_rs_XYZ = camera.convert_to_XYZ(water_mean_RGB, sky_mean_RGB, card_mean_RGB, R_rs)
+R_rs_XYZ = camera.convert_to_XYZ(R_rs)
 R_rs_XYZ_covariance = camera.XYZ_matrix @ R_rs_covariance @ camera.XYZ_matrix.T
 
-radiance_RGB_to_XYZ = hc.block_diag(*[camera.XYZ_matrix]*3)
-all_mean_XYZ = radiance_RGB_to_XYZ @ all_mean_RGB
-all_mean_XYZ_covariance = radiance_RGB_to_XYZ @ all_covariance_RGB @ radiance_RGB_to_XYZ.T
-
 # Calculate xy chromaticity
-water_xy, sky_xy, card_xy, R_rs_xy = wa.convert_XYZ_to_xy(water_XYZ, sky_XYZ, card_XYZ, R_rs_XYZ)
-water_xy_covariance = wa.convert_XYZ_to_xy_covariance(all_mean_XYZ_covariance[:3,:3], water_XYZ)
+R_rs_xy = wa.convert_XYZ_to_xy(R_rs_XYZ)
 R_rs_xy_covariance = wa.convert_XYZ_to_xy_covariance(R_rs_XYZ_covariance, R_rs_XYZ)
 
 # Calculate correlation, uncertainty
 R_rs_xy_correlation = stats.correlation_from_covariance(R_rs_xy_covariance)
 R_rs_xy_uncertainty = stats.uncertainty_from_covariance(R_rs_xy_covariance)
-water_xy_correlation = stats.correlation_from_covariance(water_xy_covariance)
-water_xy_uncertainty = stats.uncertainty_from_covariance(water_xy_covariance)
 
-print("Converted to xy:", f"xy R_rs = {R_rs_xy} +- {R_rs_xy_uncertainty} (r = {R_rs_xy_correlation[0,1]:.2f})", f"xy L_u  = {water_xy} +- {water_xy_uncertainty} (r = {water_xy_correlation[0,1]:.2f})", sep="\n")
+# Calculate correlation, uncertainty
+R_rs_xy_correlation = stats.correlation_from_covariance(R_rs_xy_covariance)
+R_rs_xy_uncertainty = stats.uncertainty_from_covariance(R_rs_xy_covariance)
+
+print("Converted to xy")
 
 # Plot chromaticity
 plot.plot_xy_on_gamut_covariance(R_rs_xy, R_rs_xy_covariance)
 
 # Calculate hue angle
-water_hue, R_rs_hue = wa.convert_xy_to_hue_angle(water_xy, R_rs_xy)
-water_hue_uncertainty = wa.convert_xy_to_hue_angle_covariance(water_xy_covariance, water_xy)
+R_rs_hue = wa.convert_xy_to_hue_angle(R_rs_xy)
 R_rs_hue_uncertainty = wa.convert_xy_to_hue_angle_covariance(R_rs_xy_covariance, R_rs_xy)
-print("Calculated hue angles:", f"alpha R_rs = {R_rs_hue:.1f} +- {R_rs_hue_uncertainty:.1f} degrees", f"alpha L_u  = {water_hue:.1f} +- {water_hue_uncertainty:.1f} degrees", sep="\n")
+print("Calculated hue angles")
 
 # Convert to Forel-Ule index
-water_FU, R_rs_FU = wa.convert_hue_angle_to_ForelUle([water_hue, R_rs_hue])
-water_FU_range = wa.convert_hue_angle_to_ForelUle_uncertainty(water_hue_uncertainty, water_hue)
+R_rs_FU = wa.convert_hue_angle_to_ForelUle(R_rs_hue)
 R_rs_FU_range = wa.convert_hue_angle_to_ForelUle_uncertainty(R_rs_hue_uncertainty, R_rs_hue)
-print("Determined Forel-Ule indices:", f"FU R_rs = {R_rs_FU} [{R_rs_FU_range[0]}-{R_rs_FU_range[1]}]", f"FU L_u  = {water_FU} [{water_FU_range[0]}-{water_FU_range[1]}]", sep="\n")
+print("Determined Forel-Ule indices:")
 
 # Save the resulting vectors to file
 hc.output_latex_vector(all_mean, saveto=saveto/"vector_L_RGBG2.tex")
