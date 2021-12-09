7f6b9b4c3e4547952532d45c9c120a04856cd7b7
Olivier Burggraaff <burggraaff@strw.leidenuniv.nl>
2021-12-09 21:35:42 +0100

    Calculate uncertainty in L/R_rs

--- process_replicates.py
+++ process_replicates.py
@@ -87,90 +87,41 @@ for data_path in hc.generate_folders(folders, pattern):
 
     # Divide by the spectral bandwidths to normalise to ADU nm^-1
     data_RGBG /= effective_bandwidths[:, np.newaxis]
-    raise Exception
-
-    # Flatten the data into one long list
-    data_all = data_RGBG.reshape(12, -1)
 
     # Calculate mean values
-    all_mean = data_all.mean(axis=-1)
-    print("Calculated mean values per image, per channel")
+    mean_per_stack = data_RGBG.mean(axis=-1)
 
-    # Calculate covariance, correlation matrices for the combined radiances
-    all_covariance = np.cov(data_all)
-    max_correlation = stats.max_correlation_in_covariance_matrix(all_covariance)
-    print(f"Calculated covariance and correlation matrices. Maximum off-diagonal correlation r = {max_correlation:.2f}")
-
-    # Plot correlation coefficients
-    plot.plot_correlation_matrix_radiance(all_covariance, x1=data_all[4], y1=data_all[5], x1label=f"{plot.keys_latex['Lsky']} (R)", y1label=f"{plot.keys_latex['Lsky']} (G)", x2=data_all[1], y2=data_all[9], x2label=f"{plot.keys_latex['Lu']} (G)", y2label=f"{plot.keys_latex['Ld']} (G)", saveto=data_path/"correlation_raw.pdf")
+    # Flatten the data into one long list
+    means_flattened = mean_per_stack.ravel()
 
     # Average G and G2
-    all_mean_RGB = hc.convert_RGBG2_to_RGB(all_mean)
-    all_covariance_RGB = hc.convert_RGBG2_to_RGB_covariance(all_covariance)
-    water_mean_RGB, sky_mean_RGB, card_mean_RGB = hc.split_combined_radiances(all_mean_RGB)
+    means_flattened_RGB = hc.convert_RGBG2_to_RGB(means_flattened)
+    mean_per_stack_RGB = means_flattened_RGB.reshape(3, nr_replicates, 3)
 
-    # Add Rref to covariance matrix
-    all_covariance_RGB_Rref = hc.add_Rref_to_covariance(all_covariance_RGB)
-
-    # Calculate Ed from Ld
-    Ld_covariance_RGB = all_covariance_RGB[-3:, -3:]  # Take only the Ld-Ld elements from the covariance matrix
-    Ed = hc.convert_Ld_to_Ed(card_mean_RGB)
-    Ed_covariance = hc.convert_Ld_to_Ed_covariance(Ld_covariance_RGB, Ed)
+    # Calculate the mean and uncertainty in RGB
+    all_mean = mean_per_stack_RGB.mean(axis=1)
+    all_std = mean_per_stack_RGB.std(axis=1)
+    uncertainty_relative = 100 * all_std/all_mean
+    print("Calculated mean values and uncertainties per image, per channel")
 
     # Convert to remote sensing reflectances
-    R_rs = hc.R_RS(water_mean_RGB, sky_mean_RGB, card_mean_RGB)
-    R_rs_covariance = hc.R_rs_covariance(all_covariance_RGB_Rref, R_rs, card_mean_RGB)
+    R_rs = hc.R_RS(*mean_per_stack_RGB)
     print("Calculated remote sensing reflectances")
 
-    # Derive naive uncertainty and correlation from covariance
-    R_rs_uncertainty = stats.uncertainty_from_covariance(R_rs_covariance)  # Uncertainty per band, ignoring covariance
-
-
-    # HydroColor
-    for reflectance, reflectance_uncertainty, c in zip(R_rs, R_rs_uncertainty, "RGB"):
-        print(f"R_rs({c}) = ({reflectance:.3f} +- {reflectance_uncertainty:.3f}) sr^-1   ({100*reflectance_uncertainty/reflectance:.0f}% uncertainty)")
-
-    # Plot the result
-    plot.plot_R_rs_RGB(RGB_wavelengths, R_rs, effective_bandwidths, R_rs_uncertainty)
-
-    # Calculate band ratios
-    bandratios = hc.calculate_bandratios(*R_rs)
-    bandratios_covariance = hc.calculate_bandratios_covariance(*R_rs, R_rs_covariance)
-    hc.print_bandratios(bandratios, bandratios_covariance)
-
-    # WACODI
-
-    # Convert RGB to XYZ
-    R_rs_XYZ = camera.convert_to_XYZ(R_rs)
-    R_rs_XYZ_covariance = camera.XYZ_matrix @ R_rs_covariance @ camera.XYZ_matrix.T
-
-    # Calculate xy chromaticity
-    R_rs_xy = wa.convert_XYZ_to_xy(R_rs_XYZ)
-    R_rs_xy_covariance = wa.convert_XYZ_to_xy_covariance(R_rs_XYZ_covariance, R_rs_XYZ)
-
-    # Calculate correlation, uncertainty
-    R_rs_xy_correlation = stats.correlation_from_covariance(R_rs_xy_covariance)
-    R_rs_xy_uncertainty = stats.uncertainty_from_covariance(R_rs_xy_covariance)
-
-    print("Converted to xy:", f"xy R_rs = {R_rs_xy} +- {R_rs_xy_uncertainty} (r = {R_rs_xy_correlation[0,1]:.2f})")
-
-    # Plot chromaticity
-    plot.plot_xy_on_gamut_covariance(R_rs_xy, R_rs_xy_covariance)
-
-    # Calculate hue angle
-    R_rs_hue = wa.convert_xy_to_hue_angle(R_rs_xy)
-    R_rs_hue_uncertainty = wa.convert_xy_to_hue_angle_covariance(R_rs_xy_covariance, R_rs_xy)
-    print("Calculated hue angles:", f"alpha R_rs = {R_rs_hue:.1f} +- {R_rs_hue_uncertainty:.1f} degrees")
-
-    # Convert to Forel-Ule index
-    R_rs_FU = wa.convert_hue_angle_to_ForelUle(R_rs_hue)
-    R_rs_FU_range = wa.convert_hue_angle_to_ForelUle_uncertainty(R_rs_hue_uncertainty, R_rs_hue)
-    print("Determined Forel-Ule indices:", f"FU R_rs = {R_rs_FU} [{R_rs_FU_range[0]}-{R_rs_FU_range[1]}]")
-
+    # Calculate mean values
+    R_rs_mean = R_rs.mean(axis=0)
+    R_rs_std = R_rs.std(axis=0)
+    uncertainty_relative_R_rs = 100 * R_rs_std/R_rs_mean
+    print("Calculated mean values and uncertainties per image, per channel, for R_rs")
 
     # Create a timestamp from EXIF (assume time zone UTC+2)
     UTC = hc.UTC_timestamp(water_exif, data_path)
 
+    # Put all data into a single array
+    data = [[UTC.timestamp(), UTC.isoformat(), *np.ravel(uncertainty_relative), *np.ravel(uncertainty_relative_R_rs)]]
+    header = ["UTC", "UTC (ISO)"] + hc.extend_keys_to_RGB(["Lu", "Lsky", "Ld", "R_rs"])
+    data_table = hc.table.Table(rows=data, names=header)
+
     # Write the result to file
-    saveto = data_path.with_name(data_path.stem + "_raw.csv")
-    hc.write_results(saveto, UTC, all_mean_RGB, all_covariance_RGB, Ed, Ed_covariance, R_rs, R_rs_covariance, bandratios, bandratios_covariance, R_rs_xy, R_rs_xy_covariance, R_rs_hue, R_rs_hue_uncertainty, R_rs_FU, R_rs_FU_range)
+    saveto = data_path.with_name(data_path.stem + "_replicates.csv")
+    data_table.write(saveto, format="ascii.fast_csv")
