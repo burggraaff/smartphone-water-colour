Refactor hyperspectral averaging - Also fixes related bug

compare_phone_reference.py
@@ -101,16 +101,8 @@ for row in table_phone:  # Loop over the smartphone table to look for matches
     reference_time = hc.iso_timestamp(table_reference[closest]["UTC"])
 
     # Calculate the median Lu/Lsky/Ed/R_rs within the matching observations, and uncertainty on this spectrum
-    row_reference = table.Table(table_reference[closest])
-    for key in hy.parameters:
-        # Average over the "close enough" rows
-        keys = [f"{key}_{wvl:.1f}" for wvl in wavelengths] + hc.extend_keys_to_RGB(key)
-        keys_err = [f"{key}_err_{wvl:.1f}" for wvl in wavelengths] + hc.extend_keys_to_RGB(key+"_err")
-
-        row_reference[keys][0] = [np.nanmedian(table_reference[k][close_enough]) for k in keys]
-        uncertainties = np.array([np.nanstd(table_reference[k][close_enough]) for k in keys])
-        row_uncertainties = table.Table(data=uncertainties, names=keys_err)
-        row_reference = table.hstack([row_reference, row_uncertainties])
+    default_index = np.where(close_enough == closest)[0][0]
+    row_reference = hy.average_hyperspectral_data(table_reference[close_enough], default_row=default_index, wavelengths=wavelengths)
 
     # If the uncertainties on the reference data are above a threshold, disregard this match-up
     # This may cause differences between RAW and JPEG matchup numbers for the same data set
@@ -133,8 +125,8 @@ for row in table_phone:  # Loop over the smartphone table to look for matches
     saveto = f"results/{ref_small}_comparison/{camera.name}_{data_type}_{phone_time}.pdf".replace(":", "-")
 
     # Plot the spectrum for comparison
-    R_rs_reference = np.array([row_reference[f"R_rs_{wvl:.1f}"][0] for wvl in wavelengths])
-    R_rs_reference_uncertainty = np.array([row_reference[f"R_rs_err_{wvl:.1f}"][0] for wvl in wavelengths])
+    R_rs_reference = hy.convert_columns_to_array(row_reference, hy.extend_keys_to_wavelengths("R_rs", wavelengths))[0]
+    R_rs_reference_uncertainty = hy.convert_columns_to_array(row_reference, hy.extend_keys_to_wavelengths("R_rs_err", wavelengths))[0]
 
     R_rs_phone = list(row[hc.extend_keys_to_RGB("R_rs")])
     R_rs_phone_err = list(row[hc.extend_keys_to_RGB("R_rs_err")])

compare_references.py
@@ -66,26 +66,14 @@ for timestamp, index_table1 in zip(table_data1_timestamps, table_data1_timestamp
     if nr_matches1 < 1 or nr_matches2 < 1:  # If no close enough matches are found, skip this observation
         continue
 
-    print(timestamp, nr_matches1, nr_matches2, min_time_diff1, min_time_diff2)
-    continue
-
     # Calculate the median Lu/Lsky/Ed/R_rs within the matching observations, and uncertainty on this spectrum
-    row_reference = table.Table(table_reference[closest])
-    for key in parameters:
-        # Average over the "close enough" rows
-        keys = [f"{key}_{wvl:.1f}" for wvl in wavelengths] + hc.extend_keys_to_RGB(key)
-        keys_err = [f"{key}_err_{wvl:.1f}" for wvl in wavelengths] + hc.extend_keys_to_RGB(key+"_err")
+    default_index1 = np.where(close_enough1 == closest1)[0][0]
+    default_index2 = np.where(close_enough2 == closest2)[0][0]
+    data1_averaged = hy.average_hyperspectral_data(table_data1[close_enough1], default_row=default_index1, colour_keys=[*"XYZxy", "sR", "sG", "sB", "hue", "FU"])
+    data2_averaged = hy.average_hyperspectral_data(table_data2[close_enough2], default_row=default_index2, colour_keys=[*"XYZxy", "sR", "sG", "sB", "hue", "FU"])
 
-        row_reference[keys][0] = [np.nanmedian(table_reference[k][close_enough]) for k in keys]
-        uncertainties = np.array([np.nanstd(table_reference[k][close_enough]) for k in keys])
-        row_uncertainties = table.Table(data=uncertainties, names=keys_err)
-        row_reference = table.hstack([row_reference, row_uncertainties])
-
-    # If the uncertainties on the reference data are above a threshold, disregard this match-up
-    # This may cause differences between RAW and JPEG matchup numbers for the same data set
-    threshold = 0.1  # relative
-    if any(row_reference[f"R_rs_err ({c})"]/row_reference[f"R_rs ({c})"] >= threshold for c in hc.colours):
-        continue
+    print(data1_averaged, "\n", data2_averaged)
+    continue
 
     # Add some metadata that may be used to identify the quality of the match
     metadata = table.Table(names=["nr_matches", "closest_match"], data=np.array([len(close_enough), time_differences[closest]]))

wk/hyperspectral.py
@@ -9,7 +9,7 @@ from astropy import table
 from scipy.linalg import block_diag
 from functools import partial
 
-from . import statistics as stats
+from . import statistics as stats, hydrocolor as hc
 
 # Add max_time_diff as a dictionary here, with keys corresponding to the different cases (ref-ref, ref-phone, NZ)
 
@@ -18,6 +18,7 @@ read = table.Table.read
 
 # Parameters measured by our hyperspectral sensors
 parameters = ["Ed", "Lsky", "Lu", "R_rs"]
+parameters_uncertainty = [param+"_err" for param in parameters]
 
 # Standard wavelength range to interpolate to.
 wavelengths_interpolation = np.arange(400, 701, 1)
@@ -110,3 +111,47 @@ def find_elements_within_range(data, reference_value, maximum_difference=60, key
 
     # Return the results
     return nr_matches, close_enough_indices, closest_index, minimum_difference
+
+
+def extend_keys_to_wavelengths(keys, wavelengths=wavelengths_interpolation):
+    """
+    For a given set of keys, e.g. ["Lu", "Lsky"], generate variants for each given wavelengths.
+    """
+    # If only one key was given, put it into a list
+    if isinstance(keys, str):
+        keys = [keys]
+
+    # Add suffixes
+    list_wvl = [key + "_{wvl:.1f}" for key in keys]
+    list_wavelengths_full = [[s.format(wvl=wvl) for wvl in wavelengths] for s in list_wvl]
+    list_wavelengths_flat = sum(list_wavelengths_full, start=[])
+
+    return list_wavelengths_flat
+
+
+def average_hyperspectral_data(data, *, parameters=parameters, wavelengths=wavelengths_interpolation, colour_keys=[*"RGBXYZxy", "sR", "sG", "sB", "hue", "FU"], default_row=0, func_average=np.nanmedian, func_uncertainty=np.nanstd):
+    """
+    Calculate the average (default: median) across multiple rows of a given data table.
+    The uncertainties are also estimated using np.nanstd by default.
+
+    The columns being averaged are any that match the pattern "{key}_{wvl:.1f}" for all keys in `parameters` and wavelengths in `wavelengths`, as well as any that match "{key} ({key_colour})" for all keys in `parameters` and `colour_keys`.
+    """
+    # Create a copy of the data table with a single placeholder row
+    data_averaged = table.Table(data[default_row])
+
+    # Get the keys that need to be averaged over
+    parameters_err = [param+"_err" for param in parameters]
+    keys = extend_keys_to_wavelengths(parameters, wavelengths=wavelengths) + hc.extend_keys_to_RGB(parameters, colour_keys)
+    keys_err = extend_keys_to_wavelengths(parameters_err, wavelengths=wavelengths) + hc.extend_keys_to_RGB(parameters_err, colour_keys)
+
+    # Calculate the averages - this needs to be done in a loop to allow in-place editing of the astropy table
+    for k in keys:
+        data_averaged[0][k] = func_average(data[k])
+
+    # Calculate the uncertainties and put them into a new table
+    uncertainties = np.array([func_uncertainty(data[k]) for k in keys])
+    table_uncertainties = table.Table(data=uncertainties, names=keys_err)
+
+    # Combine the averages and uncertainties and return the result
+    data_averaged = table.hstack([data_averaged, table_uncertainties])
+    return data_averaged